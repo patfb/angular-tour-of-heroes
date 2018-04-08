ad731ab4121ba9c74d49e63c8633d6f57e2442bf
Patrick <patrick.f.boyne@gmail.com>
2018-04-07 20:51:34 -0400

    hopeful fix for routing

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,7 @@
-import { Location } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+
 import { Hero } from './hero';
-import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
@@ -16,7 +18,7 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    //Todo: send the message _after_ fetching the hero
+    // Todo: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
